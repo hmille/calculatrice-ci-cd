f8a4fd581631b7e8d20a998e6b134c4733feaed8
Hippolyte <hippolyte.mille@etu.unilim.fr>
2025-09-11 16:28:24 +0200

    mise à jour des tests

--- tests/test_calculatrice.py
+++ tests/test_calculatrice.py
@@ -1,14 +1,18 @@
+import pytest
 from src import calculatrice
 
 def test_addition():
     assert calculatrice.addition(2, 3) == 5
 
 def test_soustraction():
-    assert calculatrice.soustraction(5, 3) == 2
+    assert calculatrice.soustraction(10, 4) == 6
 
 def test_multiplication():
-    assert calculatrice.multiplication(2, 3) == 6
+    assert calculatrice.multiplication(3, 7) == 21
 
 def test_division():
-    assert calculatrice.division(10, 2) == 5
+    assert calculatrice.division(8, 2) == 4
 
+def test_division_par_zero():
+    with pytest.raises(ValueError):
+        calculatrice.division(5, 0)
